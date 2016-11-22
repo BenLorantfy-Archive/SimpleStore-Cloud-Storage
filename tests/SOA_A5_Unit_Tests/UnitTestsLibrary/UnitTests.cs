65c8d8ac00aa7ff20ab18184c974078502693bfc
Amshar Basheer <amsharb@gmail.com>
2016-11-21 20:54:28 -0500

    add unit tests solution with completed n1 and e1

---
+++ tests/SOA_A5_Unit_Tests/UnitTestsLibrary/UnitTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTestsLibrary
+{
+    public class UnitTests
+    {
+        [Fact]
+        public void test1()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void test2()
+        {
+            Assert.True(false);
+        }
+    }
+}
